457e34bb9359c04470f67e38f6c73c61dd29c758
irlgabriel <radu.gabriel131@gmail.com>
2020-07-31 20:07:09 +0300

    Flight now range from now to 5 days forward

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -4,8 +4,8 @@ class FlightsController < ApplicationController
     @airports = Airport.all
     if params[:to_airport_id] and params[:from_airport_id]
       @date = Time.new(params[:date][:year], params[:date][:month], params[:date][:day], 0,0,0)
-      @available_flights = Flight.where(to_airport_id: params[:to_airport_id], from_airport_id: params[:from_airport_id]).to_ary.extract! {|f| f.departure_time > @date }
-      
+      @available_flights = Flight.where(to_airport_id: params[:to_airport_id], from_airport_id: params[:from_airport_id]).to_ary.extract! {|f| f.departure_time.month == @date.month }
+      @byebug
     else
       @available_flights = []
     end
@@ -15,6 +15,6 @@ class FlightsController < ApplicationController
   private
 
   def flight_params
-    params.require(:flight).permit(:to_airport_id, :from_airport_id, :travel_duration, :departure_time, :passengers)
+    params.require(:flight).permit(:to_airport_id, :from_airport_id, :travel_duration, :departure_time, :passengers, :pick)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -16,16 +16,13 @@ Airport.create(code: 'CGN')
 Airport.create(code: 'BLQ')
 Airport.create(code: 'LAX')
 Airport.create(code: 'DXB')
-Airport.create(code: 'PEK')
-Airport.create(code: 'HND')
-Airport.create(code: 'SEA')
-Airport.create(code: 'LAS')
+
 
 
 665.times do 
-  @a1 = Airport.all[rand(11)]
-  @a2 = Airport.all[rand(11)]
-  @dt = Time.at(((Time.now + 25.days).to_f - (Time.now + 5.days).to_f)*rand + (Time.now + 5.days).to_f)
+  @a1 = Airport.all[rand(7)]
+  @a2 = Airport.all[rand(7)]
+  @dt = Time.at(((Time.now + 25.days).to_f - (Time.now).to_f)*rand + (Time.now).to_f)
   @td = "#{rand(10)}:#{rand(60)}"
   Flight.create(from_airport_id: @a1.id,
                 to_airport_id: @a2.id,

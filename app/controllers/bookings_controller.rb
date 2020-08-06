08e5fb726e6c3e097355c8f854c82f1746161994
irlgabriel <radu.gabriel131@gmail.com>
2020-08-06 13:55:37 +0300

    fixed associations finally

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,20 +2,27 @@ class BookingsController < ApplicationController
 
   def new
     @flight = Flight.find(params[:flight_id])
-    @from = Airport.find(@flight.from_airport_id)
-    @to = Airport.find(@flight.to_airport_id)
-    @passengers = params[:passengers]
+    @from = Airport.find(@flight.from_airport_id).code
+    @to = Airport.find(@flight.to_airport_id).code
+    @passengers = params[:passengers].to_i
     @booking = @flight.bookings.new
   end
 
   def create
-    @flight = Flight.find(flight_id)
-    @booking = @flight.Bookings.create(booking_params)
+    @flight = Flight.find(params[:booking][:flight_id])
+    @booking = @flight.bookings.create(booking_params)
+    @from = Airport.find(@flight.from_airport_id).code
+    @to = Airport.find(@flight.to_airport_id).code
     if @booking.save 
-      redirect_to @booking
+      redirect_to @bookings #index
     else
       render "new"
     end
+  end
+
+  def index
+    @bookings = Booking.all
+
   end
 
   private

5a63cd6d015e8fcb6c8c7636c3141cc1079a6ae8
irlgabriel <radu.gabriel131@gmail.com>
2020-08-11 14:28:12 +0300

    prepared for heroku deployment

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -4,8 +4,8 @@ class FlightsController < ApplicationController
 
   def index
     @airports = Airport.all
-    @to = Airport.first.code
-    @from = Airport.first.code
+    @to = Airport.first
+    @from = Airport.first
     if params[:to_airport_id] and params[:from_airport_id]
       @date = Time.new(params[:flight]["departure_time(1i)"].to_i, params[:flight]["departure_time(2i)"].to_i, params[:flight]["departure_time(3i)"].to_i)
       @available_flights = Flight.where(to_airport_id: params[:to_airport_id], from_airport_id: params[:from_airport_id]).to_ary.extract! {|f| f.departure_time.day == @date.day }

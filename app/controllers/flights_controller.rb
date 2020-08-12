fe2aef40fb80b5a5dcb494b6ecb14ca46f6d43b8
irlgabriel <radu.gabriel131@gmail.com>
2020-08-12 15:43:04 +0300

    styled search form

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -6,6 +6,7 @@ class FlightsController < ApplicationController
     @airports = Airport.all
     @to = Airport.first
     @from = Airport.first
+    @date_now = Time.new(Time.now.year, Time.now.month, Time.now.day)
     if params[:to_airport_id] and params[:from_airport_id]
       @date = Time.new(params[:flight]["departure_time(1i)"].to_i, params[:flight]["departure_time(2i)"].to_i, params[:flight]["departure_time(3i)"].to_i)
       @available_flights = Flight.where(to_airport_id: params[:to_airport_id], from_airport_id: params[:from_airport_id]).to_ary.extract! {|f| f.departure_time.day == @date.day }
